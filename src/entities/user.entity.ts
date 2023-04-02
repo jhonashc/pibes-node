bedfcce46f52487d0cdbe553e2729447f3c909ce
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-04-01 23:06:35 -0500

    user entity updated: now person field are not optional

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -29,7 +29,9 @@ export class User extends Base {
   avatarUrl?: string;
 
   /* Relations */
-  @OneToOne(() => Person, (person) => person.user)
+  @OneToOne(() => Person, (person) => person.user, {
+    nullable: false,
+  })
   @JoinColumn({ name: "person_id" })
-  person?: Person;
+  person: Person;
 }
