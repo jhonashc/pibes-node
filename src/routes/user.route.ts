030be8a21a0c29da4256ee215a33db757ecd37d5
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-04-01 22:41:58 -0500

    user route updated: the validate request middleware was implemented for the body and params

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,13 +1,24 @@
 import { Router } from "express";
 
 import { UserController } from "../controllers";
+import { GetUsersQueryDto, UuidParamDto } from "../dtos";
+import { ValidationType } from "../interfaces";
+import { validateRequest } from "../middlewares";
 
 const router = Router();
 
 const userController = new UserController();
 
-router.get("/", userController.getUsers);
+router.get(
+  "/",
+  validateRequest(GetUsersQueryDto, ValidationType.QUERY),
+  userController.getUsers
+);
 
-router.get("/:id", userController.getUserById);
+router.get(
+  "/:id",
+  validateRequest(UuidParamDto, ValidationType.PARAMS),
+  userController.getUserById
+);
 
 export default router;
