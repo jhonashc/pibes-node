bd65256cd82efde88005e613e26c242773045eb5
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-04-01 23:06:56 -0500

    user controller updated: create user added

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,11 +1,72 @@
 import { NextFunction, Request, Response } from "express";
 
-import { GetUsersQueryDto } from "../dtos";
-import { User } from "../entities";
-import { NotFoundException } from "../exceptions";
-import { UserService } from "../services";
+import { CreateUserDto, GetUsersQueryDto } from "../dtos";
+import { Gender, User } from "../entities";
+import { ConflictException, NotFoundException } from "../exceptions";
+import { GenderService, UserService } from "../services";
 
 export class UserController {
+  async createUser(req: Request, res: Response, next: NextFunction) {
+    try {
+      const {
+        firstName,
+        lastName,
+        telephone,
+        genderId,
+        username,
+        email,
+        password,
+        avatarUrl,
+        roleIds,
+      } = req.body as CreateUserDto;
+
+      const userFound: User | null = await UserService.getUserByEmail(email);
+
+      if (userFound) {
+        throw new ConflictException(
+          `The user with the email ${email} already exists`
+        );
+      }
+
+      const genderFound: Gender | null = await GenderService.getGenderById(
+        genderId
+      );
+
+      if (!genderFound) {
+        throw new NotFoundException(
+          `The gender with id ${genderId} has not been found`
+        );
+      }
+
+      const createdUserDto: CreateUserDto = {
+        firstName,
+        lastName,
+        telephone,
+        genderId,
+        username,
+        email,
+        password,
+        avatarUrl,
+        roleIds,
+      };
+
+      const createdUser: User | undefined = await UserService.createUser(
+        createdUserDto
+      );
+
+      if (!createdUser) {
+        throw new ConflictException("There was a problem creating the user");
+      }
+
+      res.status(201).json({
+        status: true,
+        data: createdUser,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
   async getUsers(req: Request, res: Response, next: NextFunction) {
     try {
       const { username, limit, offset } = req.query as GetUsersQueryDto;
