891baa5506ba5218cbc4a144f59a4254ddc4e877
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-04-04 20:20:09 -0500

    address service added: crud operations added

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,2 +1,3 @@
+export { default as AddressService } from "./address.service";
 export { default as GenderService } from "./gender.service";
 export { default as UserService } from "./user.service";
