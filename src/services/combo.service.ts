50fa0208dc5f15aa613bf1b7d1225f33151609ce
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-04-05 12:13:37 -0500

    combo service added: create, get combos and get combo by id added

---
+++ src/services/combo.service.ts
@@ -0,0 +1,65 @@
+import {
+  FindOptionsWhere,
+  LessThanOrEqual,
+  Like,
+  MoreThanOrEqual,
+  Repository,
+} from "typeorm";
+
+import { AppDataSource } from "../config";
+import { CreateComboDto, GetCombosQueryDto } from "../dtos";
+import { Combo } from "../entities";
+
+class ComboService {
+  private readonly comboRepository: Repository<Combo>;
+
+  constructor() {
+    this.comboRepository = AppDataSource.getRepository(Combo);
+  }
+
+  createCombo(createComboDto: CreateComboDto): Promise<Combo> {
+    return this.comboRepository.save(createComboDto);
+  }
+
+  getCombos(getCombosQueryDto: GetCombosQueryDto): Promise<Combo[]> {
+    const { name, min, max, limit = 10, offset = 0 } = getCombosQueryDto;
+
+    const findOptionsWhere: FindOptionsWhere<Combo> = {};
+
+    if (name) {
+      findOptionsWhere.name = Like(`%${name}%`);
+    }
+
+    if (min) {
+      findOptionsWhere.price = LessThanOrEqual(min);
+    }
+
+    if (max) {
+      findOptionsWhere.price = MoreThanOrEqual(max);
+    }
+
+    return this.comboRepository.find({
+      where: findOptionsWhere,
+      take: limit,
+      skip: offset,
+    });
+  }
+
+  getComboById(id: string): Promise<Combo | null> {
+    return this.comboRepository.findOne({
+      where: {
+        id,
+      },
+    });
+  }
+
+  getComboByName(name: string): Promise<Combo | null> {
+    return this.comboRepository.findOne({
+      where: {
+        name: Like(name),
+      },
+    });
+  }
+}
+
+export default new ComboService();
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,6 @@
 export { default as AddressService } from "./address.service";
 export { default as CategoryService } from "./category.service";
+export { default as ComboService } from "./combo.service";
 export { default as GenderService } from "./gender.service";
 export { default as ProductService } from "./product.service";
 export { default as UserService } from "./user.service";
