57c210563d47c45c60cf4527bdc4b429d5fb70c0
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-04-04 23:37:45 -0500

    product service added: create and get products added

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,5 @@
 export { default as AddressService } from "./address.service";
 export { default as CategoryService } from "./category.service";
 export { default as GenderService } from "./gender.service";
+export { default as ProductService } from "./product.service";
 export { default as UserService } from "./user.service";
---
+++ src/services/product.service.ts
@@ -0,0 +1,107 @@
+import {
+  FindOptionsWhere,
+  LessThanOrEqual,
+  Like,
+  MoreThanOrEqual,
+  Repository,
+} from "typeorm";
+
+import { AppDataSource } from "../config";
+import { CreateProductDto, GetProductsQueryDto } from "../dtos";
+import { Product, ProductCategory } from "../entities";
+
+class ProductService {
+  private readonly productRepository: Repository<Product>;
+  private readonly productCategoryRepository: Repository<ProductCategory>;
+
+  constructor() {
+    this.productRepository = AppDataSource.getRepository(Product);
+    this.productCategoryRepository =
+      AppDataSource.getRepository(ProductCategory);
+  }
+
+  createProduct(createProductDto: CreateProductDto): Promise<Product> {
+    const {
+      name,
+      description,
+      imageUrl,
+      price,
+      stock,
+      categoryIds = [],
+    } = createProductDto;
+
+    const newProduct: Product = this.productRepository.create({
+      name,
+      description,
+      imageUrl,
+      price,
+      stock,
+      categories: categoryIds.map((categoryId) =>
+        this.productCategoryRepository.create({
+          category: {
+            id: categoryId,
+          },
+        })
+      ),
+    });
+
+    return this.productRepository.save(newProduct);
+  }
+
+  getProducts(getProductsQueryDto: GetProductsQueryDto): Promise<Product[]> {
+    const {
+      name,
+      category,
+      min,
+      max,
+      limit = 10,
+      offset = 0,
+    } = getProductsQueryDto;
+
+    const findOptionsWhere: FindOptionsWhere<Product> = {};
+
+    if (name) {
+      findOptionsWhere.name = Like(`%${name}%`);
+    }
+
+    if (category) {
+      findOptionsWhere.categories = {
+        category: {
+          name: Like(category),
+        },
+      };
+    }
+
+    if (min) {
+      findOptionsWhere.price = LessThanOrEqual(min);
+    }
+
+    if (max) {
+      findOptionsWhere.price = MoreThanOrEqual(max);
+    }
+
+    return this.productRepository.find({
+      where: findOptionsWhere,
+      take: limit,
+      skip: offset,
+    });
+  }
+
+  getProductById(id: string): Promise<Product | null> {
+    return this.productRepository.findOne({
+      where: {
+        id,
+      },
+    });
+  }
+
+  getProductByName(name: string): Promise<Product | null> {
+    return this.productRepository.findOne({
+      where: {
+        name,
+      },
+    });
+  }
+}
+
+export default new ProductService();
