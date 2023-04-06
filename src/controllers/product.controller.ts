719db079c84fd9b3e625eaecbc7dd74d51e1fb13
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-04-05 21:58:46 -0500

    product controller updated: update and delete product by id added

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,6 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 
-import { CreateProductDto, GetProductsQueryDto } from "../dtos";
+import {
+  CreateProductDto,
+  GetProductsQueryDto,
+  UpdateProductDto,
+} from "../dtos";
 import { Category, Product } from "../entities";
 import { ConflictException, NotFoundException } from "../exceptions";
 import { mapProduct, mapProducts } from "../helpers";
@@ -109,6 +113,96 @@ export class ProductController {
       }
 
       const mappedProduct: ProductMapped = mapProduct(productFound);
+
+      res.json({
+        status: true,
+        data: mappedProduct,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async updateProductById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id } = req.params;
+      const { name, description, imageUrl, price, stock, categoryIds } =
+        req.body as UpdateProductDto;
+
+      let newCategoryIds: string[] = [];
+
+      const productFound: Product | null = await ProductService.getProductById(
+        id
+      );
+
+      if (!productFound) {
+        throw new NotFoundException(
+          `The product with id ${id} has not been found`
+        );
+      }
+
+      if (!categoryIds?.length) {
+        const defaultCategory: Category | null =
+          await CategoryService.getCategoryByName("all");
+
+        if (!defaultCategory) {
+          throw new NotFoundException(
+            `The category with the name all has not been found`
+          );
+        }
+
+        newCategoryIds.push(defaultCategory.id);
+      } else {
+        const categoriesFound: Category[] =
+          await CategoryService.getCategoriesByIds(categoryIds);
+
+        if (categoriesFound.length !== categoryIds.length) {
+          throw new NotFoundException("The id of some category is invalid");
+        }
+
+        newCategoryIds = categoryIds;
+      }
+
+      const updateProductDto: UpdateProductDto = {
+        name,
+        description,
+        imageUrl,
+        price,
+        stock,
+        categoryIds: newCategoryIds,
+      };
+
+      const updatedProduct: Product | undefined =
+        await ProductService.updateProductById(productFound, updateProductDto);
+
+      res.status(201).json({
+        status: true,
+        data: updatedProduct,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async deleteProductById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id } = req.params;
+
+      const productFound: Product | null = await ProductService.getProductById(
+        id
+      );
+
+      if (!productFound) {
+        throw new NotFoundException(
+          `The product with id ${id} has not been found`
+        );
+      }
+
+      const deletedProduct: Product = await ProductService.deleteProductById(
+        productFound
+      );
+
+      const mappedProduct: ProductMapped = mapProduct(deletedProduct);
 
       res.json({
         status: true,
