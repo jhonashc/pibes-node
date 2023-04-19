023bfce2b02e2c39b01180337bfd48440998788e
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-04-19 16:09:53 -0500

    order service added: create and get orders added

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,5 +2,6 @@ export { default as AddressService } from "./address.service";
 export { default as CategoryService } from "./category.service";
 export { default as ComboService } from "./combo.service";
 export { default as GenderService } from "./gender.service";
+export { default as OrderService } from "./order.service";
 export { default as ProductService } from "./product.service";
 export { default as UserService } from "./user.service";
---
+++ src/services/order.service.ts
@@ -0,0 +1,77 @@
+import { FindOptionsWhere, Like, Repository } from "typeorm";
+
+import { AppDataSource } from "../config";
+import { CreateOrderDto, GetOrdersQueryDto } from "../dtos";
+import { Combo, Order, OrderDetail, Product, User } from "../entities";
+
+class OrderService {
+  private readonly comboRepository: Repository<Combo>;
+  private readonly orderRepository: Repository<Order>;
+  private readonly orderDetailRepository: Repository<OrderDetail>;
+  private readonly productRepository: Repository<Product>;
+  private readonly userRepository: Repository<User>;
+
+  constructor() {
+    this.comboRepository = AppDataSource.getRepository(Combo);
+    this.orderRepository = AppDataSource.getRepository(Order);
+    this.orderDetailRepository = AppDataSource.getRepository(OrderDetail);
+    this.productRepository = AppDataSource.getRepository(Product);
+    this.userRepository = AppDataSource.getRepository(User);
+  }
+
+  createOrder(CreateOrderDto: CreateOrderDto): Promise<Order> {
+    const { paymentMethod, orderStatus, userId, subtotal, total, details } =
+      CreateOrderDto;
+
+    const newOrder: Order = this.orderRepository.create({
+      paymentMethod,
+      orderStatus,
+      user: this.userRepository.create({
+        id: userId,
+      }),
+      subtotal,
+      total,
+      details: details.map(({ id, isCombo, quantity, price }) => {
+        if (isCombo) {
+          return this.orderDetailRepository.create({
+            combo: this.comboRepository.create({
+              id,
+            }),
+            quantity,
+            price,
+          });
+        }
+
+        return this.orderDetailRepository.create({
+          product: this.productRepository.create({
+            id,
+          }),
+          quantity,
+          price,
+        });
+      }),
+    });
+
+    return this.orderRepository.save(newOrder);
+  }
+
+  getOrders(getOrdersQueryDto: GetOrdersQueryDto): Promise<Order[]> {
+    const { user, limit = 10, offset = 0 } = getOrdersQueryDto;
+
+    const findOptionsWhere: FindOptionsWhere<Order> = {};
+
+    if (user) {
+      findOptionsWhere.user = {
+        username: Like(user.toLowerCase()),
+      };
+    }
+
+    return this.orderRepository.find({
+      where: findOptionsWhere,
+      take: limit,
+      skip: offset,
+    });
+  }
+}
+
+export default new OrderService();
