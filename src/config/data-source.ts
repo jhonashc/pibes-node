de2cda7d82d4497aab634fd371e89db29b4e5f5e
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-04-18 20:09:56 -0500

    config updated: entities path for dev and production fixed in data source

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,5 +1,7 @@
 import { DataSource } from "typeorm";
 
+const BASE_DIR = process.env.NODE_ENV === "dev" ? "src" : "dist";
+
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.HOST || "localhost",
@@ -8,7 +10,7 @@ export const AppDataSource = new DataSource({
   password: process.env.DB_PASSWORD || "postgres",
   database: process.env.DB_NAME || "pibesdb",
   synchronize: process.env.NODE_ENV === "dev" ? true : false,
-  entities: ["dist/**/*.entity{.ts,.js}", "src/**/*.entity{.ts,.js}"],
+  entities: [`${BASE_DIR}/**/*.entity{.ts,.js}`],
   subscribers: [],
   migrations: [],
 });
