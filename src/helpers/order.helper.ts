fe65ea3bc7ea47009f353635752c6b48fc119f54
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-04-19 21:39:59 -0500

    combo helper updated: refactored with new order mapped interfaces

--- src/helpers/order.helper.ts
+++ src/helpers/order.helper.ts
@@ -1,19 +1,25 @@
 import { Order } from "../entities";
 import { mapCombo, mapProduct } from "../helpers";
-import { OrderMapped } from "../interfaces";
+import { ComboMapped, OrderMapped, ProductMapped } from "../interfaces";
 
-export const mapOrder = (order: Order): OrderMapped => {
+export const mapOrder = (
+  order: Order
+): OrderMapped<ComboMapped | ProductMapped> => {
   return {
     ...order,
-    details: order.details.map(({ combo, product, quantity, price }) => ({
-      combo: combo && mapCombo(combo),
-      product: product && mapProduct(product),
-      quantity,
-      price,
-    })),
+    details: order.details.map(({ combo, product, quantity, price }) => {
+      return {
+        item: combo ? mapCombo(combo) : mapProduct(product!),
+        isCombo: combo ? true : false,
+        quantity,
+        price,
+      };
+    }),
   };
 };
 
-export const mapOrders = (orders: Order[]): OrderMapped[] => {
+export const mapOrders = (
+  orders: Order[]
+): OrderMapped<ComboMapped | ProductMapped>[] => {
   return orders.map((order) => mapOrder(order));
 };
