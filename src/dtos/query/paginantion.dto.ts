3d0a5abaef1a242f12e06ee6917e753c719ae28f
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-04-20 12:28:09 -0500

    query dto updated: minimum to the limit and a maximum to the offset added in pagination query dto

--- src/dtos/query/paginantion.dto.ts
+++ src/dtos/query/paginantion.dto.ts
@@ -4,12 +4,14 @@ import { IsOptional, IsPositive, Max, Min } from "class-validator";
 export class PaginationQueryDto {
   @IsOptional()
   @IsPositive()
+  @Min(5)
   @Max(20)
   @Type(() => Number)
   limit?: number;
 
   @IsOptional()
   @Min(0)
+  @Max(20)
   @Type(() => Number)
   offset?: number;
 }
