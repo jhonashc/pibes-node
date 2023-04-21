e88d8a13a0486c65e8185b975e523a544a6dc717
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-04-21 03:34:51 -0500

    address service updated: refactored and moved all functions related to the user to an own service

--- src/services/address.service.ts
+++ src/services/address.service.ts
@@ -1,13 +1,6 @@
 import { Repository } from "typeorm";
 
 import { AppDataSource } from "../config";
-
-import {
-  CreateAddressDto,
-  GetAddressesQueryDto,
-  UpdateAddressDto,
-} from "../dtos";
-
 import { Address } from "../entities";
 
 class AddressService {
@@ -17,64 +10,12 @@ class AddressService {
     this.addressService = AppDataSource.getRepository(Address);
   }
 
-  createAddress(createAddressDto: CreateAddressDto): Promise<Address> {
-    const { addressLine1, addressLine2, addressReference, userId } =
-      createAddressDto;
-
-    const newAddress: Address = this.addressService.create({
-      addressLine1,
-      addressLine2,
-      addressReference,
-      user: {
-        id: userId,
-      },
-    });
-
-    return this.addressService.save(newAddress);
-  }
-
-  getUserAddress(
-    getAddressesQueryDto: GetAddressesQueryDto
-  ): Promise<Address[]> {
-    const { userId, limit = 10, offset = 0 } = getAddressesQueryDto;
-
-    return this.addressService.find({
-      where: {
-        user: {
-          id: userId,
-        },
-      },
-      take: limit,
-      skip: offset,
-    });
-  }
-
   getAddressById(id: string): Promise<Address | null> {
     return this.addressService.findOne({
       where: {
         id,
       },
     });
-  }
-
-  updateAddressById(
-    address: Address,
-    updateAddressDto: UpdateAddressDto
-  ): Promise<Address> {
-    const { addressLine1, addressLine2, addressReference } = updateAddressDto;
-
-    const newAddress: Address = this.addressService.create({
-      id: address.id,
-      addressLine1,
-      addressLine2,
-      addressReference,
-    });
-
-    return this.addressService.save(newAddress);
-  }
-
-  deleteAddressById(address: Address): Promise<Address> {
-    return this.addressService.remove(address);
   }
 }
 
