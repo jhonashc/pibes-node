9fdb88be9517294469b1563b1a4076fdeea14b8a
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-05-11 22:00:52 -0500

    promotion route updated: endpoint to get promotion with products added

--- src/routes/promotion.route.ts
+++ src/routes/promotion.route.ts
@@ -1,10 +1,36 @@
 import { Router } from "express";
+
 import { PromotionController } from "../controllers";
+
+import {
+  CreatePromotionDto,
+  GetPromotionsQueryDto,
+  GetPromotionsWithProductsQueryDto,
+} from "../dtos";
+
+import { ValidationType } from "../interfaces";
+import { validateRequest } from "../middlewares";
 
 const router = Router();
 
 const promotionController = new PromotionController();
 
-router.get("/", promotionController.getPromotions);
+router.post(
+  "/",
+  validateRequest(CreatePromotionDto),
+  promotionController.createPromotion
+);
+
+router.get(
+  "/",
+  validateRequest(GetPromotionsQueryDto, ValidationType.QUERY),
+  promotionController.getPromotions
+);
+
+router.get(
+  "/products",
+  validateRequest(GetPromotionsWithProductsQueryDto, ValidationType.QUERY),
+  promotionController.getPromotionsWithProducts
+);
 
 export default router;
