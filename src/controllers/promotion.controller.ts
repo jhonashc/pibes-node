7977c69c0a738dd3226477dcf0a667ef36624764
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-05-11 22:00:39 -0500

    promotion controller updated: get promotion with products added

--- src/controllers/promotion.controller.ts
+++ src/controllers/promotion.controller.ts
@@ -1,27 +1,109 @@
 import { NextFunction, Request, Response } from "express";
 
-import { GetPromotionsQueryDto } from "../dtos";
-import { Promotion } from "../entities";
-import { PromotionMapped } from "../interfaces";
+import {
+  CreatePromotionDto,
+  GetPromotionsQueryDto,
+  GetPromotionsWithProductsQueryDto,
+} from "../dtos";
+import { Product, Promotion } from "../entities";
+import { ConflictException, NotFoundException } from "../exceptions";
 import { mapPromotions } from "../helpers";
-import { PromotionService } from "../services";
+import { PromotionMapped } from "../interfaces";
+import { ProductService, PromotionService } from "../services";
 
 export class PromotionController {
+  async createPromotion(req: Request, res: Response, next: NextFunction) {
+    try {
+      const {
+        name,
+        description,
+        discountPercentage,
+        availableDay,
+        productIds,
+      } = req.body as CreatePromotionDto;
+
+      const filteredName: string = name.trim().toLowerCase();
+
+      const promotionFound: Promotion | null =
+        await PromotionService.getPromotionByName(filteredName);
+
+      if (promotionFound) {
+        throw new ConflictException(
+          `The promotion with the name ${name} already exists`
+        );
+      }
+
+      if (productIds) {
+        const productsFound: Product[] = await ProductService.getProductsByIds(
+          productIds
+        );
+
+        if (productsFound.length !== productIds.length) {
+          throw new NotFoundException("The id of some product is invalid");
+        }
+      }
+
+      const createPromotionDto: CreatePromotionDto = {
+        name,
+        description,
+        discountPercentage,
+        availableDay,
+        productIds,
+      };
+
+      const createdPromotion: Promotion =
+        await PromotionService.createPromotion(createPromotionDto);
+
+      res.status(201).json({
+        status: true,
+        data: createdPromotion,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
   async getPromotions(req: Request, res: Response, next: NextFunction) {
     try {
-      const { limit, offset } = req.query as GetPromotionsQueryDto;
+      const { day, limit, offset } = req.query as GetPromotionsQueryDto;
 
       const promotions: Promotion[] = await PromotionService.getPromotions({
+        day,
         limit,
         offset,
       });
 
-      const mappedPromotions: PromotionMapped[] = mapPromotions(promotions);
-
       res.json({
         status: true,
-        data: mappedPromotions,
+        data: promotions,
       });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async getPromotionsWithProducts(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    const { day, limit, offset } =
+      req.query as GetPromotionsWithProductsQueryDto;
+
+    const promotions: Promotion[] =
+      await PromotionService.getPromotionsWithProducts({
+        day,
+        limit,
+        offset,
+      });
+
+    const mappedPromotions: PromotionMapped[] = mapPromotions(promotions);
+
+    res.json({
+      status: true,
+      data: mappedPromotions,
+    });
+    try {
     } catch (error) {
       next(error);
     }
