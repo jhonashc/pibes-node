697330306257b33f8c30b2138bac3093b42387c3
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-05-16 23:01:49 -0500

    helpers updated: mappers added

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -2,7 +2,14 @@ export { comparePasswords, encryptPassword } from "./bcrypt.helper";
 export { getCurrentDay } from "./date.helper";
 export { newUserEmailTemplate } from "./email.helper";
 export { generateTokens, verifyToken } from "./jwt.helper";
-export { mapOrder, mapOrders } from "./order.helper";
+
+export {
+  mapOrder,
+  mapOrders,
+  mapProduct,
+  mapProducts,
+  mapPromotion,
+  mapPromotions,
+} from "./mappers.helper";
+
 export { generateOpt } from "./otp.helper";
-export { mapProduct, mapProducts } from "./product.helper";
-export { mapPromotion, mapPromotions } from "./promotion.helper";
---
+++ src/helpers/mappers.helper.ts
@@ -0,0 +1,38 @@
+import { Order, Product, Promotion } from "../entities";
+import { OrderMapped, ProductMapped, PromotionMapped } from "../interfaces";
+
+export const mapOrder = (order: Order): OrderMapped => {
+  return {
+    ...order,
+    details: order.details.map(({ product, quantity }) => ({
+      product: mapProduct(product),
+      quantity,
+    })),
+  };
+};
+
+export const mapOrders = (orders: Order[]): OrderMapped[] => {
+  return orders.map((order) => mapOrder(order));
+};
+
+export const mapProduct = (product: Product): ProductMapped => {
+  return {
+    ...product,
+    categories: product.categories.map(({ category }) => category),
+  };
+};
+
+export const mapProducts = (products: Product[]): ProductMapped[] => {
+  return products.map((product) => mapProduct(product));
+};
+
+export const mapPromotion = (promotion: Promotion): PromotionMapped => {
+  return {
+    ...promotion,
+    products: promotion.products.map(({ product }) => mapProduct(product)),
+  };
+};
+
+export const mapPromotions = (promotions: Promotion[]): PromotionMapped[] => {
+  return promotions.map((promotion) => mapPromotion(promotion));
+};
