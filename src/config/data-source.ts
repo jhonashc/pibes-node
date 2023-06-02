2e94fc84a3b3580f45980b4ddcc04dbb5b47f90b
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-06-02 15:48:17 -0500

    config updated: migrations path added

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -8,7 +8,7 @@ export const AppDataSource = new DataSource({
   port: parseInt(process.env.DB_PORT || "5432"),
   username: process.env.DB_USER || "postgres",
   password: process.env.DB_PASSWORD || "postgres",
-  database: process.env.DB_NAME || "pibesdb",
-  synchronize: process.env.NODE_ENV === "dev" ? true : false,
-  entities: [`${BASE_DIR}/**/*.entity{.ts,.js}`],
+  database: process.env.DB_NAME || "lospibesdb",
+  entities: [`${BASE_DIR}/entities/*.entity{.ts,.js}`],
+  migrations: [`${BASE_DIR}/migrations/*{.ts,.js}`],
 });
