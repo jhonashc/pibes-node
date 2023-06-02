f5b3eb7751cbdf3ce78a7128c1aea9930aea43dc
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-06-02 15:49:56 -0500

    user address dto updated: create and update user address dto updated

--- src/dtos/user-address/create-user-address.dto.ts
+++ src/dtos/user-address/create-user-address.dto.ts
@@ -1,15 +1,15 @@
-import { IsNotEmpty, IsString } from "class-validator-multi-lang";
+import { IsNotEmpty, IsOptional, IsString } from "class-validator-multi-lang";
 
 export class CreateUserAddressDto {
   @IsString()
   @IsNotEmpty()
-  addressLine1: string;
+  name: string;
 
   @IsString()
   @IsNotEmpty()
-  addressLine2: string;
+  sideStreet: string;
 
   @IsString()
   @IsNotEmpty()
-  addressReference: string;
+  deliveryInstruction: string;
 }
--- src/dtos/user-address/update-user-address.dto.ts
+++ src/dtos/user-address/update-user-address.dto.ts
@@ -1,18 +1,15 @@
-import { IsNotEmpty, IsOptional, IsString } from "class-validator-multi-lang";
+import { IsOptional, IsString } from "class-validator-multi-lang";
 
 export class UpdateUserAddressDto {
   @IsString()
-  @IsNotEmpty()
   @IsOptional()
-  addressLine1?: string;
+  name?: string;
 
   @IsString()
-  @IsNotEmpty()
   @IsOptional()
-  addressLine2?: string;
+  sideStreet?: string;
 
   @IsString()
-  @IsNotEmpty()
   @IsOptional()
-  addressReference?: string;
+  deliveryInstruction?: string;
 }
