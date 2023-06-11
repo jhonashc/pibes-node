d347616143276978df67701f255f762134ecab7a
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-06-11 16:28:05 -0500

    file route updated: endpoint to upload image removed

--- src/routes/file.route.ts
+++ src/routes/file.route.ts
@@ -1,13 +1,10 @@
 import { Router } from "express";
 
 import { FileController } from "../controllers";
-import { uploader } from "../middlewares";
 
 const router = Router();
 
 const fileController = new FileController();
-
-router.post("/", uploader.single("file"), fileController.uploadImage);
 
 router.get("/:imageName", fileController.getImageByName);
 
