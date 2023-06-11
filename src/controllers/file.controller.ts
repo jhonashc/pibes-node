dd961bf1f992beb1c1304a170013ff335bfdc943
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-06-11 17:30:35 -0500

    file controller updated: delete image by name added

--- src/controllers/file.controller.ts
+++ src/controllers/file.controller.ts
@@ -22,4 +22,28 @@ export class FileController {
       next(error);
     }
   }
+
+  async deleteImageByName(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { imageName } = req.params;
+
+      const imagePath: string = FileService.getImageByName(imageName);
+
+      if (!existsSync(imagePath)) {
+        throw new BadRequestException(
+          `La imagen con el nombre ${imageName} no ha sido encontrada`
+        );
+      }
+
+      await FileService.deleteImageByName(imagePath);
+
+      res.json({
+        status: true,
+        message: "La imagen ha sido eliminada con éxito",
+        data: imagePath,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
 }
