385278938a974371992f5409b8607e557c7d9534
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-06-16 02:18:32 -0500

    query dto updated: pagination query dto refactored

--- src/dtos/query/paginantion.dto.ts
+++ src/dtos/query/paginantion.dto.ts
@@ -2,15 +2,14 @@ import { Type } from "class-transformer";
 import { IsOptional, IsPositive, Max, Min } from "class-validator-multi-lang";
 
 export class PaginationQueryDto {
+  @IsPositive()
   @IsOptional()
+  @Type(() => Number)
+  page?: number;
+
   @Min(5)
   @Max(20)
-  @IsPositive()
+  @IsOptional()
   @Type(() => Number)
   limit?: number;
-
-  @IsOptional()
-  @Min(0)
-  @Type(() => Number)
-  offset?: number;
 }
