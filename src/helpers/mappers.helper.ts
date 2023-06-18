fb750213ed1e42f0e418520e20d540370b88f2a5
jhonashc <jhonhuiracocha123456789@gmail.com>
2023-06-17 22:16:01 -0500

    mappers helper updated: map order and product refactored

--- src/helpers/mappers.helper.ts
+++ src/helpers/mappers.helper.ts
@@ -1,9 +1,18 @@
-import { Order, Product } from "../entities";
+import { DayOfWeek, Order, Product } from "../entities";
 import { OrderMapped, ProductMapped } from "../interfaces";
 
+import { getPromotionDay } from "./promotion.helper";
+
 export const mapOrder = (order: Order): OrderMapped => {
+  const { user } = order;
+
   return {
     ...order,
+    user: {
+      username: user.username,
+      email: user.email,
+      avatarUrl: user.avatarUrl,
+    },
     items: order.items.map(({ product, quantity }) => ({
       item: mapProduct(product),
       quantity,
@@ -16,11 +25,15 @@ export const mapOrders = (orders: Order[]): OrderMapped[] => {
 };
 
 export const mapProduct = (product: Product): ProductMapped => {
+  const currentDay: DayOfWeek = getPromotionDay();
+
   return {
     ...product,
-    images: product.images?.map(({ url }) => url),
+    images: product.images.map(({ url }) => url),
     categories: product.categories.map(({ category }) => category),
-    promotions: product.promotions?.map(({ promotion }) => promotion),
+    promotions: product.promotions
+      .filter(({ promotion }) => promotion.availableDays.includes(currentDay))
+      .map(({ promotion }) => promotion),
   };
 };
 
